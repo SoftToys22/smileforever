25e458f29edf7fdc84c71e3233a9011a8fd459b9
Bhavik Devaiya <bdevaiya@gmail.com>
2022-09-05 21:56:44 +0530

    13

--- sb/server.js
+++ sb/server.js
@@ -13,7 +13,6 @@ mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   })
   .then(() => {
     console.log('connected to db');
