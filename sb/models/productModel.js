c1682a32d701b4e5ce5a046a1341149f75fbaac2
Bhavik Devaiya <bdevaiya@gmail.com>
2022-09-06 21:46:16 +0530

    6

--- sb/models/productModel.js
+++ sb/models/productModel.js
@@ -1,5 +1,17 @@
 import mongoose from 'mongoose';
 
+const reviewSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    name: { type: String, required: true },
+    rating: { type: Number },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
