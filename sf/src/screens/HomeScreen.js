21a53078c4b8af3e0866b8f9c29758f5387b0c34
Bhavik Devaiya <bdevaiya@gmail.com>
2022-09-10 14:16:27 +0530

    55

--- sf/src/screens/HomeScreen.js
+++ sf/src/screens/HomeScreen.js
@@ -49,7 +49,7 @@ function HomeScreen() {
         <title>Smile Forever</title>
       </Helmet>
       <h1>Featured Products</h1>
-      <div className="product">
+      <div className="products">
         {loading ? (
           <LoadingBox />
         ) : error ? (
