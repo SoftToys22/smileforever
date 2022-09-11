e7f20ab1d33b282bfcc952b48cc1f0d7cd355bd2
Bhavik Devaiya <bdevaiya@gmail.com>
2022-09-11 21:45:17 +0530

    45

--- sf/src/components/AdminRoute.js
+++ sf/src/components/AdminRoute.js
@@ -1,9 +1,7 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext } from 'react';s
 import { Store } from '../Store';
 
 export default function AdminRoute({ children }) {
-  const navigate = useNavigate();
   const { state } = useContext(Store);
   const { userInfo } = state;
   return userInfo && userInfo.isAdmin ? children : <navigate to="/signin" />;
