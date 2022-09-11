b6613987a5362b7861d3b175a30035c0aa944be6
Bhavik Devaiya <bdevaiya@gmail.com>
2022-09-11 21:24:01 +0530

    96

--- sf/src/components/ProtectedRoute.js
+++ sf/src/components/ProtectedRoute.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Store } from '../Store';
 
 export default function AdminRoute({ children }) {
+  const navigate = useNavigate();
   const { state } = useContext(Store);
   const { userInfo } = state;
-  return userInfo && userInfo.isAdmin ? children : <Navigate to="/signin" />;
+  return userInfo && userInfo.isAdmin ? children : <navigate to="/signin" />;
 }
