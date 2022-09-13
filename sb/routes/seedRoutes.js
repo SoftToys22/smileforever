edc2a1a889c7d787563d7970770d9cf261984b6a
Bhavik Devaiya <bdevaiya@gmail.com>
2022-09-13 22:43:01 +0530

    67

--- sb/routes/seedRoutes.js
+++ sb/routes/seedRoutes.js
@@ -7,7 +7,7 @@ const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
   await Product.remove({});
-  const createdProducts = await Product.insertMany(data?.products);
+  const createdProducts = await Product.insertMany(data.products);
   await User.remove({});
   const createdUsers = await User.insertMany(data.users);
   res.send({ createdProducts, createdUsers });
