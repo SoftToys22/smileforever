336c219d58958b6a87484f6f0161ee35f45fce3e
Bhavik Devaiya <bdevaiya@gmail.com>
2022-09-17 20:50:07 +0530

    35

--- sf/src/screens/HomeScreen.js
+++ sf/src/screens/HomeScreen.js
@@ -58,7 +58,7 @@ function HomeScreen() {
           <Row>
             {products.map((product) => (
               <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
-                <Product product={product}></Product>
+                <Product product={products}></Product>
               </Col>
             ))}
           </Row>
